0f72bccd2e22a2f751cffaa6ac2eb4e1cf02ffbf
Rutujasnp <65347296+Rutujasnp@users.noreply.github.com>
2024-03-05 21:24:35 -0800

    commit 1

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+ data={ title :'Angular core deep dive'};
+
+ onLogoClicked(){
+
+  alert('Hello world');
+ }
+ onKeyUp(newTitle: string){
+this.data.title = newTitle;
+ }
+}
